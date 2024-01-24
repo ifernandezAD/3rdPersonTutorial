5d8fdb35ed58909532880f07c67a1992cc96ed32
IFernandezAD <ifernandezad@gmail.com>
2024-01-24 15:39:12 +0100

    Force Receiver

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -6,7 +6,7 @@ public class PlayerStateMachine : StateMachine
 {
     [field : SerializeField] public InputReader InputReader { get; private set;}
 
-    [field: SerializeField] public CharacterController CharacterController { get; private set; }
+    [field: SerializeField] public CharacterController Controller { get; private set; }
 
     [field: SerializeField] public Animator Animator { get; private set; }
 
@@ -16,8 +16,9 @@ public class PlayerStateMachine : StateMachine
 
     [field: SerializeField] public float RotationDamping { get; private set; }
 
-    public Transform MainCameraTransform { get; private set; }
+    [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
 
+    public Transform MainCameraTransform { get; private set; }
 
 
     private void Start()
