64fb02be34d9b2587e8db31e062f9dbab107baf0
IFernandezAD <ifernandezad@gmail.com>
2024-01-29 15:28:33 +0100

    Combo Attacks

--- Assets/Scripts/StateMachines/Player/Attack.cs
+++ Assets/Scripts/StateMachines/Player/Attack.cs
@@ -7,4 +7,10 @@ using System;
 public class Attack 
 {
     [field: SerializeField] public string AnimationName { get; private set; }
+
+    [field: SerializeField] public float TransitionDuration { get; private set; }
+
+    [field: SerializeField] public int ComboStateIndex { get; private set; } = -1;
+
+    [field: SerializeField] public float ComboAttackTime { get; private set; }
 }
