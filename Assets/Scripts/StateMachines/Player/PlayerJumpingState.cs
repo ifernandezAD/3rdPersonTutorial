cb67a0ec0cbb3da32eeb4241e2f1d2f930625038
IFernandezAD <ifernandezad@gmail.com>
2024-02-06 10:56:21 +0100

    Start working on jump

---
+++ Assets/Scripts/StateMachines/Player/PlayerJumpingState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpingState : PlayerBaseState
+{
+    public PlayerJumpingState(PlayerStateMachine stateMachine) : base(stateMachine) {}
+      
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
